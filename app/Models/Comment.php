f16f2bf1a0ee3e48af7562d51c0ae39f1346f937
Dilshan <ndkumarawansha@gmail.com>
2024-09-23 14:26:58 +0530

    migrations and models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'post_id',
+        'user_id',
+    ];
 }
