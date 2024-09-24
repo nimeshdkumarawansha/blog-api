82b600b90c8398451295a5c8f6a7f64985de8773
Dilshan <ndkumarawansha@gmail.com>
2024-09-24 20:09:38 +0530

    seeders added

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+          // Generate 100 posts
+        Comment::factory()->count(100)->create();
+    }
+}
